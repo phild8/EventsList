22f0d8c762bb0e361f8bd5e6f8a02b8e90acdd6a
Phild <pjsdavs@gmail.com>
2017-09-21 15:56:30 -0700

    Added Asset Folder ---Added Images Added EventDetailsActivity.java

---
+++ app/src/main/java/com/example/pdavis11/eventslist/EventDetailsActivity.java
@@ -0,0 +1,57 @@
+package com.example.pdavis11.eventslist;
+
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class EventDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_details);
+
+        // Get the data out of the intent
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+        String details = intent.getStringExtra("details");
+
+        ImageView eventImageView = (ImageView) findViewById(R.id.eventImageView);
+
+        // Create references to the text views
+        TextView titleTextView = (TextView) findViewById(R.id.eventTitleTextView);
+        TextView detailsTitleView = (TextView) findViewById(R.id.eventDetailsTextView);
+
+        // Set the text of the the text views
+        titleTextView.setText(title);
+        detailsTitleView.setText(details);
+
+        // Use the asset manager to retrieve a file (image)
+        AssetManager am = this.getAssets();
+        String imageFileName = title.replace(" ", "") + ".jpeg";
+        // Use AssetManager to open a stream to the file name.
+        try{
+            InputStream stream = am.open(imageFileName);
+            Drawable image = Drawable.createFromStream(stream, title);
+            eventImageView.setImageDrawable(image);
+        } catch (IOException e){
+            Log.e("OC Music Events", "Error loading image:" + imageFileName);
+        }
+
+    }
+
+    protected void goBackToList(View v)
+    {
+        // Terminates the current activity (terminates the details activity)
+        finish();
+    }
+}
--- app/src/main/java/com/example/pdavis11/eventslist/EventListActivity.java
+++ app/src/main/java/com/example/pdavis11/eventslist/EventListActivity.java
@@ -1,13 +1,39 @@
 package com.example.pdavis11.eventslist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class EventListActivity extends AppCompatActivity {
+public class EventListActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_event_list);
+        // Content has already been set for ListActivity
+       // setContentView(R.layout.activity_event_list);
+
+        // Define a built-in list adapter for this ListActivity:
+        setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, MusicEvent.titles));
+
     }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+
+        // Use the position in the list to look up in the titles array
+        String title = MusicEvent.titles[position];
+        String details = MusicEvent.details[position];
+
+        // Create an intent to go to DetailsActivity with title and details sent
+        Intent detailsIntent = new Intent(this, EventDetailsActivity.class);
+        detailsIntent.putExtra("title", title); // K V
+        detailsIntent.putExtra("details", details); // K V
+
+        startActivity(detailsIntent);
+
+    }
+
 }
